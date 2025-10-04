f1167ea538c0bb20e30320f7a701faf676080c2b
shrawan <shrawan29yadav@gmail.com>
2025-10-04 12:59:21 +0530

    deployment ready

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import {client} from "@repo/db/client"
+
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/',function(req,res){
+    res.send("hii there");
+})
+
+app.post('/signup',async (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+   const user = await client.user.create({
+        data:{
+            username:username,
+            password:password
+        }
+    })
+
+    res.json({
+        msg:"you are signedin baby",
+        id:user.id
+    })
+})
+
+app.listen(3002);
